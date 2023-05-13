8b270ddf20173d96ddda753f45189ad657b63c23
KsenosNick <xenos-nick@mail.ru>
2023-05-13 19:47:15 +0300

    Начало работы с Vue

--- kish/src/App.vue
+++ kish/src/App.vue
@@ -6,38 +6,16 @@
       <div class="content">
         <img src="./assets/img/banner.jpg" alt="Logo" />
         <ul class="content__list">
-          <li>
-            <span class="content__song">Кукла колдуна</span>
-            <button class="content__like">
+          <li v-for="item in items" :key="item.id">
+            <span class="content__song">{{ items.song }}</span>
+            <button @click="incrementLikes(item)" class="content__like">
               <svg viewBox="0 0 24 24" fill="none">
                 <path
                   d="M12 5.881C12.981 4.729 14.484 4 16.05 4C18.822 4 21 6.178 21 8.95C21 12.3492 17.945 15.1195 13.3164 19.3167L13.305 19.327L12 20.515L10.695 19.336L10.6595 19.3037C6.04437 15.1098 3 12.3433 3 8.95C3 6.178 5.178 4 7.95 4C9.516 4 11.019 4.729 12 5.881Z"
                 />
               </svg>
             </button>
-            <span class="content__count">5</span>
-          </li>
-          <li>
-            <span class="content__song">Лесник</span>
-            <button class="content__like">
-              <svg viewBox="0 0 24 24" fill="none">
-                <path
-                  d="M12 5.881C12.981 4.729 14.484 4 16.05 4C18.822 4 21 6.178 21 8.95C21 12.3492 17.945 15.1195 13.3164 19.3167L13.305 19.327L12 20.515L10.695 19.336L10.6595 19.3037C6.04437 15.1098 3 12.3433 3 8.95C3 6.178 5.178 4 7.95 4C9.516 4 11.019 4.729 12 5.881Z"
-                />
-              </svg>
-            </button>
-            <span class="content__count">5</span>
-          </li>
-          <li>
-            <span class="content__song">Мёртвый анархист</span>
-            <button class="content__like">
-              <svg viewBox="0 0 24 24" fill="none">
-                <path
-                  d="M12 5.881C12.981 4.729 14.484 4 16.05 4C18.822 4 21 6.178 21 8.95C21 12.3492 17.945 15.1195 13.3164 19.3167L13.305 19.327L12 20.515L10.695 19.336L10.6595 19.3037C6.04437 15.1098 3 12.3433 3 8.95C3 6.178 5.178 4 7.95 4C9.516 4 11.019 4.729 12 5.881Z"
-                />
-              </svg>
-            </button>
-            <span class="content__count">5</span>
+            <span class="content__count">{{ item.likes }}</span>
           </li>
         </ul>
       </div>
@@ -45,4 +23,13 @@
   </div>
 </template>
 
-<script setup></script>
+<script>
+export default (await import('vue')).defineAsyncComponent({
+  data() {
+    return {
+      items: [],
+    };
+  },
+  methods: {},
+});
+</script>
