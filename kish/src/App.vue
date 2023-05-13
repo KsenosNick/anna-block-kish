1e3ff33150c070a5be53173b3360bab93bee4176
KsenosNick <xenos-nick@mail.ru>
2023-05-13 19:28:08 +0300

    Перенос вёрстки

--- kish/src/App.vue
+++ kish/src/App.vue
@@ -1,30 +1,48 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <div class="left-anim"></div>
+    <div class="right-anim"></div>
+    <div class="container">
+      <div class="content">
+        <img src="./assets/img/banner.jpg" alt="Logo" />
+        <ul class="content__list">
+          <li>
+            <span class="content__song">Кукла колдуна</span>
+            <button class="content__like">
+              <svg viewBox="0 0 24 24" fill="none">
+                <path
+                  d="M12 5.881C12.981 4.729 14.484 4 16.05 4C18.822 4 21 6.178 21 8.95C21 12.3492 17.945 15.1195 13.3164 19.3167L13.305 19.327L12 20.515L10.695 19.336L10.6595 19.3037C6.04437 15.1098 3 12.3433 3 8.95C3 6.178 5.178 4 7.95 4C9.516 4 11.019 4.729 12 5.881Z"
+                />
+              </svg>
+            </button>
+            <span class="content__count">5</span>
+          </li>
+          <li>
+            <span class="content__song">Лесник</span>
+            <button class="content__like">
+              <svg viewBox="0 0 24 24" fill="none">
+                <path
+                  d="M12 5.881C12.981 4.729 14.484 4 16.05 4C18.822 4 21 6.178 21 8.95C21 12.3492 17.945 15.1195 13.3164 19.3167L13.305 19.327L12 20.515L10.695 19.336L10.6595 19.3037C6.04437 15.1098 3 12.3433 3 8.95C3 6.178 5.178 4 7.95 4C9.516 4 11.019 4.729 12 5.881Z"
+                />
+              </svg>
+            </button>
+            <span class="content__count">5</span>
+          </li>
+          <li>
+            <span class="content__song">Мёртвый анархист</span>
+            <button class="content__like">
+              <svg viewBox="0 0 24 24" fill="none">
+                <path
+                  d="M12 5.881C12.981 4.729 14.484 4 16.05 4C18.822 4 21 6.178 21 8.95C21 12.3492 17.945 15.1195 13.3164 19.3167L13.305 19.327L12 20.515L10.695 19.336L10.6595 19.3037C6.04437 15.1098 3 12.3433 3 8.95C3 6.178 5.178 4 7.95 4C9.516 4 11.019 4.729 12 5.881Z"
+                />
+              </svg>
+            </button>
+            <span class="content__count">5</span>
+          </li>
+        </ul>
+      </div>
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<script setup></script>
